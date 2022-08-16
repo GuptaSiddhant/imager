0eae7304036eb55665f36653bbc01bef1ce2c2a6
Siddhant Gupta <siddhant.c.gupta@accenture.com>
2022-08-16 11:06:15 +0300

    Add vercel zlib setup

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -6,6 +6,17 @@ import { renderToPipeableStream } from "react-dom/server";
 
 const ABORT_DELAY = 5000;
 
+if (
+  process.env.LD_LIBRARY_PATH == null ||
+  !process.env.LD_LIBRARY_PATH.includes(
+    `${process.env.PWD}/node_modules/canvas/build/Release:`
+  )
+) {
+  process.env.LD_LIBRARY_PATH = `${
+    process.env.PWD
+  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ""}`;
+}
+
 export default function handleRequest(
   request: Request,
   responseStatusCode: number,
