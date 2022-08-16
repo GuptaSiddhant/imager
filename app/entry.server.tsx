e9080a313e4d90697c245a81bdd0ae9ef422d91e
Siddhant Gupta <siddhant.c.gupta@accenture.com>
2022-08-16 11:28:50 +0300

    Move vercel config to server

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -3,6 +3,21 @@ import { Response } from "@remix-run/node";
 import { RemixServer } from "@remix-run/react";
 import { renderToString } from "react-dom/server";
 
+function setupVercel() {
+  if (
+    process.env.LD_LIBRARY_PATH == null ||
+    !process.env.LD_LIBRARY_PATH.includes(
+      `${process.env.PWD}/node_modules/canvas/build/Release:`
+    )
+  ) {
+    process.env.LD_LIBRARY_PATH = `${
+      process.env.PWD
+    }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ""}`;
+  }
+}
+
+setupVercel();
+
 export default function handleRequest(
   request: Request,
   responseStatusCode: number,
